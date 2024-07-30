44020f55582775a53e983dd7d77c088a3e76bdcd
t-wilkinson <winston.trey.wilkinson@gmail.com>
2024-07-30 09:41:52 -0400

    Add nixos-unstable branch

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
