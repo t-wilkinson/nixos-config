f2353a7457bbb93e6e09f82f63800e281db5df83
t-wilkinson <winston.trey.wilkinson@gmail.com>
2025-05-15 06:12:51 +0700

    Add CirnOS

---
+++ CirnOS/flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "end_4's NixOS flake";
+
+  outputs = { self, impurity, ... }: {
+    # editing flake.nix triggers certain utilities such as direnv
+    # to reload - editing host configurations do not require a direnv
+    # reload, so lets move hosts out of the way
+    nixosConfigurations = import ./hosts { inherit self; };
+  };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    impurity.url = "github:outfoxxed/impurity.nix";
+    thorium.url = "github:end-4/nix-thorium";
+
+    hyprland.url = "github:hyprwm/Hyprland";
+    hyprland-plugins = {
+      url = "github:hyprwm/hyprland-plugins";
+      # inputs.nixpkgs.follows = "hyprland";
+    };
+
+    ags.url = "github:Aylur/ags";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    gross = {
+      url = "github:fufexan/gross";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-parts.follows = "flake-parts";
+    };
+    matugen = {
+      url = "github:/InioX/Matugen";
+      # ref = "refs/tags/matugen-v0.10.0"
+    };
+    more-waita = {
+      url = "github:somepaulo/MoreWaita";
+      flake = false;
+    };
+    firefox-gnome-theme = {
+      url = "github:rafaelmardojai/firefox-gnome-theme";
+      flake = false;
+    };
+    anyrun = {
+      url = "github:Kirottu/anyrun";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+}
---
+++ CirnOS/hosts/CirnOS/default.nix
@@ -0,0 +1,10 @@
+{
+  imports = [
+    ./audio.nix
+    ./hardware-configuration.nix
+    ./configuration.nix
+    ./gnome.nix
+    # ./laptop.nix
+    ./locale.nix
+  ];
+}
