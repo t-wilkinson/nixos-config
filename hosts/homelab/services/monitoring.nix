e335e9f1c53143e7a332719fc1be1baefabbce4a
t-wilkinson <winston.trey.wilkinson@gmail.com>
2026-01-20 22:24:01 -0500

    made changes

--- hosts/homelab/services/monitoring.nix
+++ hosts/homelab/services/monitoring.nix
@@ -20,49 +20,49 @@ in
       };
 
       # PROMETHEUS
-      services.prometheus = {
-        enable = true;
-        port = services.prometheus.port;
-        exporters = {
-          node = {
-            enable = true;
-            enabledCollectors = [ "systemd" ];
-            port = 9100;
-          };
-        };
-        # Scrape locally
-        scrapeConfigs = [
-          {
-            job_name = "homelab";
-            static_configs = [
-              {
-                targets = [ "127.0.0.1:9100" ];
-              }
-            ];
-          }
-        ];
-      };
+      # services.prometheus = {
+      #   enable = true;
+      #   port = services.prometheus.port;
+      #   exporters = {
+      #     node = {
+      #       enable = true;
+      #       enabledCollectors = [ "systemd" ];
+      #       port = 9100;
+      #     };
+      #   };
+      #   # Scrape locally
+      #   scrapeConfigs = [
+      #     {
+      #       job_name = "homelab";
+      #       static_configs = [
+      #         {
+      #           targets = [ "127.0.0.1:9100" ];
+      #         }
+      #       ];
+      #     }
+      #   ];
+      # };
 
       # GRAFANA
-      services.grafana = {
-        enable = true;
-        settings = {
-          server = {
-            http_addr = "127.0.0.1";
-            http_port = services.grafana.port;
-            domain = services.grafana.domain;
-          };
-        };
-        # Automatically add Prometheus as a data source
-        provision.datasources.settings.datasources = [
-          {
-            name = "Prometheus";
-            type = "prometheus";
-            access = "proxy";
-            url = "http://127.0.0.1:${toString services.prometheus.port}";
-          }
-        ];
-      };
+      # services.grafana = {
+      #   enable = true;
+      #   settings = {
+      #     server = {
+      #       http_addr = "127.0.0.1";
+      #       http_port = services.grafana.port;
+      #       domain = services.grafana.domain;
+      #     };
+      #   };
+      #   # Automatically add Prometheus as a data source
+      #   provision.datasources.settings.datasources = [
+      #     {
+      #       name = "Prometheus";
+      #       type = "prometheus";
+      #       access = "proxy";
+      #       url = "http://127.0.0.1:${toString services.prometheus.port}";
+      #     }
+      #   ];
+      # };
     };
   };
 }
