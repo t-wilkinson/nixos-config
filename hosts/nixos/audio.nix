0c617d68a3e9f8375470621bd4f8b4db3ed9044b
t-wilkinson <winston.trey.wilkinson@gmail.com>
2026-03-13 23:14:07 -0400

    Audio changes

--- hosts/nixos/audio.nix
+++ hosts/nixos/audio.nix
@@ -17,27 +17,27 @@
 
     wireplumber.extraConfig = {
       "10-disable-suspend" = {
-        "monitor.bluez.properties" = {
-          "bluez5.enable-sbc-xq" = true;
-          "bluez5.enable-msbc" = true;
-          "bluez5.enable-hw-volume" = true;
-          "bluez5.roles" = [
-            "hsp_hs"
-            "hsp_ag"
-            "hfp_hf"
-            "hfp_ag"
-          ];
-        };
-        "monitor.bluez.rules" = [
-          {
-            matches = [ { "node.name" = "~bluez_output.*"; } ];
-            actions = {
-              update-props = {
-                "session.suspend-timeout-seconds" = 0; # 0 disables suspend
-              };
-            };
-          }
-        ];
+        # "monitor.bluez.properties" = {
+        #   "bluez5.enable-sbc-xq" = true;
+        #   "bluez5.enable-msbc" = true;
+        #   "bluez5.enable-hw-volume" = true;
+        #   "bluez5.roles" = [
+        #     "hsp_hs"
+        #     "hsp_ag"
+        #     "hfp_hf"
+        #     "hfp_ag"
+        #   ];
+        # };
+        # "monitor.bluez.rules" = [
+        #   {
+        #     matches = [ { "node.name" = "~bluez_output.*"; } ];
+        #     actions = {
+        #       update-props = {
+        #         "session.suspend-timeout-seconds" = 0; # 0 disables suspend
+        #       };
+        #     };
+        #   }
+        # ];
         "monitor.alsa.rules" = [
           {
             matches = [
@@ -64,13 +64,13 @@
   hardware.bluetooth = {
     enable = true;
     powerOnBoot = true;
-    settings = {
-      General = {
-        Enable = "Source,Sink,Media,Socket";
-        Experimental = true; # Enables some modern features that improve stability
-        FastConnectable = true;
-      };
-    };
+    # settings = {
+    #   General = {
+    #     Enable = "Source,Sink,Media,Socket";
+    #     Experimental = true; # Enables some modern features that improve stability
+    #     FastConnectable = true;
+    #   };
+    # };
   };
 
   systemd.services.pipewire-restart-on-resume = {
